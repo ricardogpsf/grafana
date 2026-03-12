2b01e1bbbd48bd9d1f8e44d260e94aed0c0fcd92
Ricardo Galeno <ricardogaleno.rg@gmail.com>
2026-03-12 13:02:59 -0300

    Use info text color for banner links

--- public/app/features/announcement-banner/AnnouncementBanner.tsx
+++ public/app/features/announcement-banner/AnnouncementBanner.tsx
@@ -48,7 +48,7 @@ const getStyles = (theme: GrafanaTheme2, headerHeight: number) => ({
       margin: 0,
     },
     '& a': {
-      color: 'inherit',
+      color: theme.colors.info.text,
       textDecoration: 'underline',
     },
   }),
